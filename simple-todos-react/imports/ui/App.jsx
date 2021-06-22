fe68b4f71d054dc3f03a86fb09f23ae11f6b905e
Maxim270693 <maxim.zhuk.93@mail.ru>
2021-06-22 16:26:28 +0300

    создание логинизации

--- simple-todos-react/imports/ui/App.jsx
+++ simple-todos-react/imports/ui/App.jsx
@@ -1,22 +1,58 @@
-import React from 'react';
+import React, {useState, Fragment} from 'react';
 import {Task} from "./Task";
 import {useTracker} from 'meteor/react-meteor-data';
 import {TasksCollection} from "../api/TasksCollection";
 import {TaskForm} from './TaskForm';
+import {LoginForm} from './LoginForm';
 
+
+const toggleChecked = ({_id, isChecked}) => {
+    TasksCollection.update(_id, {
+        $set: {
+            isChecked: !isChecked
+        }
+    })
+};
+
+const deleteTask = ({_id}) => TasksCollection.remove(_id);
 
 export const App = () => {
+    const user = useTracker(() => Meteor.user());
 
-    const tasks = useTracker(() => TasksCollection.find({}, {sort: {createdAt: -1}}).fetch());
-    const deleteTask = ({_id}) => TasksCollection.remove(_id);
+    const [hideCompleted, setHideCompleted] = useState(false);
 
-    const toggleChecked = ({_id, isChecked}) => {
-        TasksCollection.update(_id, {
-            $set: {
-                isChecked: !isChecked
+    const hideCompletedFilter = {isChecked: {$ne: true}};
+
+    const userFilter = user ? {userId: user._id} : {};
+
+    const pendingOnlyFilter = {...hideCompletedFilter, ...userFilter};
+
+    const tasks = useTracker(() => {
+        if (!user) {
+            return [];
+        }
+
+        return TasksCollection.find(
+            hideCompleted ? pendingOnlyFilter : userFilter,
+            {
+                sort: {createdAt: -1},
             }
-        })
-    };
+        ).fetch();
+    });
+
+    const pendingTasksCount = useTracker(() => {
+        if (!user) {
+            return 0;
+        }
+
+        return TasksCollection.find(pendingOnlyFilter).count();
+    });
+
+    const pendingTasksTitle = `${
+        pendingTasksCount ? ` (${pendingTasksCount})` : ''
+    }`;
+
+    const logout = () => Meteor.logout();
 
     return (
         <div className='app'>
@@ -24,18 +60,37 @@ export const App = () => {
                 <div className="app-bar">
                     <div className="app-header">
                         <h1>TodoList</h1>
+                        {pendingTasksTitle}
                     </div>
                 </div>
             </header>
             <div className="main">
-                <TaskForm/>
-                <ul className="tasks">
-                    {tasks.map(task => <Task key={task._id}
-                                             task={task}
-                                             onCheckboxClick={toggleChecked}
-                                             onDeleteClick={deleteTask}
-                    />)}
-                </ul>
+                {user ? (
+                    <Fragment>
+                        <div className="user" onClick={logout}>
+                            {user.username}
+                        </div>
+                        <TaskForm user={user}/>
+                        <div className="filter">
+                            <button onClick={() => setHideCompleted(!hideCompleted)}>
+                                {hideCompleted ? 'Show All' : 'Hide Completed'}
+                            </button>
+                        </div>
+
+                        <ul className="tasks">
+                            {tasks.map(task => (
+                                <Task
+                                    key={task._id}
+                                    task={task}
+                                    onCheckboxClick={toggleChecked}
+                                    onDeleteClick={deleteTask}
+                                />
+                            ))}
+                        </ul>
+                    </Fragment>
+                ) : (
+                    <LoginForm/>
+                )}
             </div>
         </div>
     )
